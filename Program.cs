0fd49a37bc836c5aaa33c348377eeeab7e787bbb
Vivek Jadhav <jadhavvivek619@gmail.com>
2021-08-29 19:16:41 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NastedIf;
+
+namespace NastedIfDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Details D = new Details();
+            D.input();
+            D.print();
+            D.oprtn();
+        }
+    }
+}
